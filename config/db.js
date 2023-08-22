d3cb4402df3601a32badae7cdf1d3ef2ba21acdf
tatvkamdar79 <kamdartatv79@gmail.com>
2023-08-22 21:34:46 +0530

    temporarily added db string

--- config/db.js
+++ config/db.js
@@ -1,7 +1,12 @@
 const mongoose = require("mongoose");
 
 // Connect to the MongoDB instance
-mongoose.connect("mongodb://localhost:27017/HyderabadHardware", {
+// mongoose.connect("mongodb://localhost:27017/HyderabadHardware", {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// });
+
+mongoose.connect("mongodb://tatv:tatv123@ac-pj6srpl-shard-00-00.yyqkb3m.mongodb.net:27017,ac-pj6srpl-shard-00-01.yyqkb3m.mongodb.net:27017,ac-pj6srpl-shard-00-02.yyqkb3m.mongodb.net:27017/?ssl=true&replicaSet=atlas-645zr9-shard-0&authSource=admin&retryWrites=true&w=majority", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
@@ -13,6 +18,7 @@ const db = mongoose.connection;
 db.on("error", console.error.bind(console, "Connection error:"));
 db.once("open", () => {
   console.log("Connected to the database.");
+  console.log(db.collections);
 });
 
 // Export the mongoose connection
