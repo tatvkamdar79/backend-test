43afd45b1c06ab2b6794b20b375d56498a7e03a0
tatvkamdar79 <kamdartatv79@gmail.com>
2023-08-24 13:25:20 +0530

    Added exec time and learnt indexing properly

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,4 +1,5 @@
 const Product = require("../models/ProductModel");
+const { sendSuccess } = require("../utils/response");
 
 exports.getAllProducts = async (req, res) => {
   const products = await Product.find();
@@ -11,11 +12,22 @@ exports.getProductBySearch = async (req, res) => {
     return res.json([]);
   }
   const searchString = req.query.searchQuery;
+  let startTime = process.hrtime();
   const searchResult = await Product.find({
     $or: [
       { tags: { $regex: searchString, $options: "i" } },
       { productTitle: { $regex: searchString, $options: "i" } },
     ],
   });
-  res.json(searchResult);
+
+  let endTime = process.hrtime(startTime);
+  function parseHrtimeToSeconds(hrtime) {
+    var seconds = (hrtime[0] + hrtime[1] / 1e9).toFixed(3);
+    return seconds;
+  }
+  console.log(parseHrtimeToSeconds(endTime));
+  return sendSuccess(res, "Fetched Documents", {
+    searchResult,
+    time: parseHrtimeToSeconds(endTime),
+  });
 };
