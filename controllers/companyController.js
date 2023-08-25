f60f3cb8e4896ab9e9884011147b8427c22f42b1
tatvkamdar79 <kamdartatv79@gmail.com>
2023-08-25 23:18:06 +0530

    Added company routes

--- controllers/companyController.js
+++ controllers/companyController.js
@@ -31,3 +31,41 @@ exports.addCompany = async (req, res) => {
     return sendError(res, error.message, error);
   }
 };
+
+exports.updateCompanyName = async (req, res) => {
+  const _id = req.body._id;
+  const updatedCompanyName = req.body.companyName;
+  if (!_id || !updatedCompanyName) {
+    return sendError(res, "Invalid Request");
+  }
+  try {
+    const company = await Company.findOne({ _id });
+    if (!company) {
+      sendError(res, `No Company Found!`);
+    }
+    company.companyName =
+      updatedCompanyName.charAt(0).toUpperCase() + updatedCompanyName.slice(1);
+    company.save();
+    return sendSuccess(res, "Company Name Updated Successfully", company);
+  } catch (err) {
+    return sendError(res, "Database Error", err);
+  }
+};
+
+exports.deleteCompany = async (req, res) => {
+  const _id = req.body._id;
+  if (!_id) {
+    return sendError(res, "Invalid Request");
+  }
+  console.log(req.body);
+  try {
+    const company = await Company.findOne({ _id });
+    if (!company) {
+      sendError(res, `No Company Found!`);
+    }
+    await company.deleteOne();
+    return sendSuccess(res, "Company Deleted Successfully", {});
+  } catch (err) {
+    return sendError(res, "Database Error", err);
+  }
+};
--- routes/companyRoutes.js
+++ routes/companyRoutes.js
@@ -4,5 +4,7 @@ const companyController = require("../controllers/companyController");
 
 router.get("/getAllCompanies", companyController.getAllCompanies);
 router.post("/addCompany", companyController.addCompany);
+router.post("/updateCompanyName", companyController.updateCompanyName);
+router.post("/deleteCompany", companyController.deleteCompany);
 
 module.exports = router;
