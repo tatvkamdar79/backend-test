737d8fe970479e9b2e8d574d52339cb92a336663
tatvkamdar79 <kamdartatv79@gmail.com>
2023-11-20 20:09:22 +0530

    Added option to add and delete google sheets

--- routes/googleSheetsRoutes.js
+++ routes/googleSheetsRoutes.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const { syncGS } = require("../googleSheetsModule/googleSheetsSync");
 const router = express.Router();
+const googleSheetsController = require("../controllers/googleSheetsController");
 
 router.post("/syncGoogleSheet", syncGS);
+router.post("/addNewSheet", googleSheetsController.addNewSheet);
+router.get("/getAllSheets", googleSheetsController.getAllSheets);
+router.post("/deleteSheet", googleSheetsController.deleteSheet);
 
 module.exports = router;
